791f4f0051aa015a80c3e9d619d5deadb3694694
OsnyNeto <osnynt@gmail.com>
2023-11-15 18:42:59 -0300

    Interface Play Station com Angular

---
+++ angular-blog/src/app/data/dataFakes.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id":1,
+    "photoCover":"https://www.bmw-motorrad.com.br/content/dam/bmwmotorradnsc/marketBR/bmw-motorrad_com_br/varejo/1250_gsa_bmw_motorrad.jpg.asset.1693407137623.jpg",
+    "title":"R 1250 GS Adventure",
+    "description":"A nova BMW R 1250 GS Adventure foi projetada para superar seus desafios de aventura. A rainha das maxitrails de viagem nasceu para andar sobre os terrenos mais difíceis, nas condições mais adversas e rumo aos destinos mais distantes. Graças ao aumento de cilindrada para 1.254 cc e a presença do sistema ShiftCam, que melhora a entrega do motor em todas as faixas de rotação, a nova BMW R 1250 GS Adventure atua de forma mais eficiente e oferece a resistência necessária para os destinos mais aventureiros. A mais nova geração do motor boxer fascina por proporcionar tamanho prazer de condução graças ao aumento das cilindradas e a um cabeçote do motor completamente redesenhado. O conjunto propulsor exclusivo ganha pontos com um desempenho visivelmente maior nos quesitos de potência e torque ao longo de toda a faixa de rotações, enquanto, ao mesmo tempo, oferece uma suavidade de funcionamento melhorada através do controle variável das válvulas, chamado de tecnologia BMW ShiftCam. "
+  },
+  {
+    "id":2,
+    "photoCover":"https://conteudo.imguol.com.br/c/colunas/0f/2022/09/30/nova-bmw-s-1000rr-1664546302912_v2_1x1.jpg",
+    "title":"BMW S 1000 RR",
+    "description":"A S 1000 RR provocou uma revolução na categoria das superesportivas no seu lançamento em 2009. Desde então, a moto continua em evolução, lapidando ainda mais seu projeto testado e aprovado por pilotos do mundo todo. Com 210 cv e torque de 113 Nm à 11.000rpm, a S 1000 RR continua arrasadora e traz o que há de mais moderno em termos de tecnologia embarcada. O resultado é uma moto com menos peso e uma melhoria evidente no desempenho.  Cada peça da RR foi concebida para extrair o melhor desempenho. Vejamos, por exemplo, a configuração da ciclística com uma nova tecnologia de suspensão que torna a RR mais leve e ainda mais precisa. Além disso, temos uma posição de condução com uma ergonomia perfeita, devido ao quadro modificado e ao design das carenagens e das asas. Os faróis LED de série conferem à motocicleta uma imagem dinâmica e agressiva. Os piscas dianteiros foram deslocados das carenagens para os espelhos retrovisores, resultando em uma nova estética, além de gerar melhor visibilidade. Vista desde a traseira, a RR domina em termos de design e funcionalidade. "
+  },
+  {
+    "id":3,
+    "photoCover":"https://swiatmotocykli.pl/wp-content/uploads/2021/06/P90386372_highRes_the-bmw-r-18-04-2020.jpg",
+    "title":"A NOVA BMW R18",
+    "description":"A nova R18 traz a nossa história e os marcos icônicos da nossa marca em que trabalhamos arduamente.  O resultado: uma Cruiser cheia de personalidade com um motor Boxer com a maior cilindrada que já construímos, além de recursos de design atemporais.  "
+  },
+  {
+    "id":4,
+    "photoCover":"https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/10/09/1782531439-c-400-x-w2.jpg",
+    "title":"BMW C 400 X",
+    "description":"Redescubra percursos urbanos com conforto e tecnologia de uma verdadeira Scooter BMW. Mais do que um meio de transporte, uma extensão do seu corpo para se conectar ainda mais com a cidade, encontrando a liberdade que ela proporciona."
+  },
+
+]
---
+++ angular-routes/src/app/pages/portifolio/card/card.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  constructor(
+    private parametrizador: ActivatedRoute
+  ) {
+    // http://localhost:4200/portfolio/{1}
+    this.parametrizador.params.subscribe(
+      res => console.log(res)
+    )
+    // http://localhost:4200/portfolio/1?name=osny&token=123
+    this.parametrizador.queryParams.subscribe(
+      res => console.log(res)
+    )
+  }
+
+  ngOnInit(): void {
+  }
+
+}
